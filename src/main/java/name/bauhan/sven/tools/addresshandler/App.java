e9e9bb506598d29cebebfb4be930484e9770d408
Sven <Sven@colin>
2013-10-25 16:24:56 +0200

    Initialized maven project

---
+++ src/main/java/name/bauhan/sven/tools/addresshandler/App.java
@@ -0,0 +1,13 @@
+package name.bauhan.sven.tools.addresshandler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
