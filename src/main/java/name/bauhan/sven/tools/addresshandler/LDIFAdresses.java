0318f5c5dbfe3ccb8cc2daa5c3207eb30dca749b
Sven Bauhan <Sven@colin>
2014-02-04 02:50:29 +0100

    Writing of LDIF files (birthday not working yet)

--- src/main/java/name/bauhan/sven/tools/addresshandler/LDIFAdresses.java
+++ src/main/java/name/bauhan/sven/tools/addresshandler/LDIFAdresses.java
@@ -1,26 +1,34 @@
 package name.bauhan.sven.tools.addresshandler;
 
+import com.novell.ldap.LDAPAttribute;
+import com.novell.ldap.LDAPAttributeSet;
+import com.novell.ldap.LDAPEntry;
+import com.novell.ldap.LDAPException;
+import com.novell.ldap.util.LDAPWriter;
+import com.novell.ldap.util.LDIFWriter;
 import java.util.List;
 import java.util.regex.Pattern;
 import ezvcard.VCard;
+import ezvcard.parameter.AddressType;
 import ezvcard.parameter.EmailType;
 import ezvcard.parameter.TelephoneType;
+import ezvcard.property.Telephone;
 import ezvcard.property.Address;
 import ezvcard.property.Birthday;
+import ezvcard.property.Email;
 import ezvcard.property.StructuredName;
 import ezvcard.util.PartialDate;
-import java.util.Calendar;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.LinkedList;
 import org.apache.directory.api.ldap.model.entry.Attribute;
 import org.apache.directory.api.ldap.model.exception.LdapInvalidAttributeValueException;
-import org.apache.directory.api.ldap.model.exception.LdapInvalidDnException;
 import org.apache.directory.api.ldap.model.ldif.LdapLdifException;
 import org.apache.directory.api.ldap.model.ldif.LdifEntry;
 import org.apache.directory.api.ldap.model.ldif.LdifReader;
-import org.apache.directory.api.ldap.model.name.Dn;
-import org.apache.directory.api.ldap.model.name.Rdn;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -129,15 +137,95 @@ public class LDIFAdresses extends AddressFile {
 		}
 	}
 
+	private void writeEntry(LDAPWriter writer, VCard vCard) throws IOException, LDAPException {
+		LDAPAttributeSet attrSet = new LDAPAttributeSet();
+		LDAPAttribute attr;
+		StructuredName name = vCard.getStructuredName();
+		attr = new LDAPAttribute("givenName", name.getGiven());
+		attrSet.add(attr);
+		attr = new LDAPAttribute("sn", name.getFamily());
+		attrSet.add(attr);
+		List<Address> addressList = vCard.getAddresses();
+		for (Address addr : addressList) {
+			if (addr.getTypes().contains(AddressType.HOME)) {
+				String street = addr.getStreetAddress();
+				if (street != null) {
+					attr = new LDAPAttribute("streetaddress", street);
+					attrSet.add(attr);
+				}
+				String postalCode = addr.getPostalCode();
+				if (postalCode != null) {
+					attr = new LDAPAttribute("postalcode", postalCode);
+					attrSet.add(attr);
+				}
+				String location = addr.getLocality();
+				if (location != null) {
+					attr = new LDAPAttribute("l", location);
+					attrSet.add(attr);
+				}
+			}
+		}
+		List<Telephone> phones = vCard.getTelephoneNumbers();
+		for (Telephone tel : phones) {
+			if (tel.getTypes().contains(TelephoneType.HOME)) {
+				attr = new LDAPAttribute("homePhone", tel.getText());
+				attrSet.add(attr);
+			} else if (tel.getTypes().contains(TelephoneType.CELL)) {
+				attr = new LDAPAttribute("mobile", tel.getText());
+				attrSet.add(attr);
+			}
+		}
+		List<Email> emailList = vCard.getEmails();
+		for (Email email : emailList) {
+			if (email.getTypes().contains(EmailType.HOME)) {
+				attr = new LDAPAttribute("email", email.getValue());
+				attrSet.add(attr);
+			}
+		}
+		Birthday birth = vCard.getBirthday();
+		if (birth != null) {
+			PartialDate partial = birth.getPartialDate();
+			if (partial != null) {
+				Integer year = partial.getYear();
+				if (year != null) {
+					attr = new LDAPAttribute("birthyear", year.toString());
+					attrSet.add(attr);
+				}
+				Integer month = partial.getMonth();
+				if (month != null) {
+					attr = new LDAPAttribute("birthmonth", month.toString());
+					attrSet.add(attr);
+				}
+				Integer day = partial.getDate();
+				if (day != null) {
+					attr = new LDAPAttribute("birthday", day.toString());
+					attrSet.add(attr);
+				}
+			}
+			Date date = birth.getDate();
+			if (date != null) {
+				
+			}
+		}
+		LDAPEntry entry = new LDAPEntry(vCard.getStructuredName().toString(), attrSet);
+		writer.writeEntry(entry);
+	}
+
 	@Override
 	public void writeFile() {
 		try {
-			Rdn rdn = new Rdn("cn", "org");
-			Dn dn = new Dn("cn=test", "ou=example", "dn=address", "dn=org");
-			LdifEntry entry = new LdifEntry(dn, "");
-		} catch (LdapInvalidAttributeValueException ex) {
-		} catch (LdapLdifException ex) {
-		} catch (LdapInvalidDnException ex) {
+			FileOutputStream fileStream = new FileOutputStream(file_name);
+			LDIFWriter writer = new LDIFWriter(fileStream);
+			for (VCard vcard : addresses) {
+				writeEntry(writer, vcard);
+			}
+			writer.finish();
+		} catch (FileNotFoundException ex) {
+			logger.error(ex.getLocalizedMessage());
+		} catch (IOException ex) {
+			logger.error(ex.getLocalizedMessage());
+		} catch (LDAPException ex) {
+			logger.error(ex.getLocalizedMessage());
 		}
 	}
 }
