b6c6b633aef61dc445ce29e556dadc9106e92cba
Sven Bauhan <Sven@colin>
2014-03-30 22:59:06 +0200

    Added some widgets for address fields

--- src/main/java/name/bauhan/sven/tools/addresshandler/MainWindowController.java
+++ src/main/java/name/bauhan/sven/tools/addresshandler/MainWindowController.java
@@ -1,7 +1,10 @@
 package name.bauhan.sven.tools.addresshandler;
 
 import ezvcard.VCard;
+import ezvcard.parameter.TelephoneType;
+import ezvcard.property.Email;
 import ezvcard.property.StructuredName;
+import ezvcard.property.Telephone;
 import java.io.File;
 import java.net.URL;
 import java.util.List;
@@ -17,6 +20,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import org.apache.commons.lang3.StringUtils;
@@ -31,7 +35,7 @@ public class MainWindowController implements Initializable {
 	final FileChooser fileChooser = new FileChooser();
 	AddressFile addr_file;
 	List<VCard> addresses;
-	
+
 	@FXML
 	MenuItem closeMenu;
 	@FXML
@@ -50,6 +54,12 @@ public class MainWindowController implements Initializable {
 	TextField givenText;
 	@FXML
 	TextField familyText;
+	@FXML
+	TextField homePhone;
+	@FXML
+	TextField cellPhone;
+	@FXML
+	TextArea emailText;
 	
 	/**
 	 * Initializes the controller class.
@@ -82,8 +92,21 @@ public class MainWindowController implements Initializable {
 		prefixText.setText(prefix);
 		givenText.setText(name.getGiven());
 		familyText.setText(name.getFamily());
+		List<Telephone> tel_numbers = current_addr.getTelephoneNumbers();
+		for (Telephone tel : tel_numbers) {
+			if (tel.getTypes().contains(TelephoneType.HOME)) {
+				homePhone.setText(tel.getText());
+			} else if (tel.getTypes().contains(TelephoneType.CELL)) {
+				cellPhone.setText(tel.getText());
+			}
+		}
+		emailText.clear();
+		List<Email> email_addresses = current_addr.getEmails();
+		for (Email mail : email_addresses) {
+			emailText.appendText(mail.getValue() + "\n");
+		}
 	}
-	
+
 	@FXML
 	private void handleOpenAction(ActionEvent event) {
 		File readFile = fileChooser.showOpenDialog(null);
@@ -124,14 +147,14 @@ public class MainWindowController implements Initializable {
 	private void handleCloseRequest(ActionEvent event_) {
 		Platform.exit();
 	}
-	
+
 	private void dataChanged(String action_, String file_) {
 		boolean isFileOpened = (addr_file != null);
 
 		// set menu disability
-		closeMenu.setDisable(! isFileOpened);
-		saveMenu.setDisable(! isFileOpened);
-		saveAsMenu.setDisable(! isFileOpened);
+		closeMenu.setDisable(!isFileOpened);
+		saveMenu.setDisable(!isFileOpened);
+		saveAsMenu.setDisable(!isFileOpened);
 
 		// Set status line
 		leftStatus.setText(action_);
@@ -152,8 +175,8 @@ public class MainWindowController implements Initializable {
 					String prefix = StringUtils.join(prefixes, " ");
 					name_str = prefix + " " + name.getGiven() + " " + name.getFamily();
 				}
-				list.add( name_str );
+				list.add(name_str);
 			}
-		}		
+		}
 	}
 }
